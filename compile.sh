a44a8234aa125f1d3d99742d26aee3b1891e3b86
Eldar Insafutdinov <insafutdin@mpi-inf.mpg.de>
2017-07-11 12:14:00 +0200

    use python3 explicitely in the compile script

--- compile.sh
+++ compile.sh
@@ -3,15 +3,15 @@
 cd lib
 
 cd coco/PythonAPI/
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 cd -
 
 cd multicut_cython
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 cd -
 
 cd nms_cython
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 cd -
 
 cd ..
