d2cfa7e23bad5c570be8a60fb5a832ba37ed21d6
Andreas Behr <andreas@codedreality.com>
2017-10-19 12:23:35 +0200

    Fixes model data download location. Closes #42
    Model data location changed to https server, for future relocation
    option -L (follow moves) has been added to the curl command.

--- models/mpii/download_models.sh
+++ models/mpii/download_models.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -O http://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.data-00000-of-00001
-curl -O http://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.meta
-curl -O http://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.index
+curl -O -L https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.data-00000-of-00001
+curl -O -L https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.meta
+curl -O -L https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.index
