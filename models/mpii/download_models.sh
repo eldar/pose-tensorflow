a3e85d375dd5c2428461f5782d32363aa7abf059
Eldar Insafutdinov <insafutdin@mpi-inf.mpg.de>
2019-03-17 14:35:52 +0100

    fix download script

--- models/mpii/download_models.sh
+++ models/mpii/download_models.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-curl -L -O https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.data-00000-of-00001
-curl -L -O https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.meta
-curl -L -O https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.index
+if command -v curl 1>/dev/null; then
+    DOWNLOADER="curl -L -O"
+else
+    DOWNLOADER="wget"
+fi
+
+$DOWNLOADER https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.data-00000-of-00001
+$DOWNLOADER https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.meta
+$DOWNLOADER https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/mpii-single-resnet-101.index
