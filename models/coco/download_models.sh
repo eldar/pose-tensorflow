29b7ad43f28730f791a3f9b5953dff5c03eff651
Eldar Insafutdinov <insafutdin@mpi-inf.mpg.de>
2019-03-17 14:44:07 +0100

    use wget when curl is not available

--- models/coco/download_models.sh
+++ models/coco/download_models.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-curl -L -O https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/coco-resnet-101.data-00000-of-00001
-curl -L -O https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/coco-resnet-101.meta
-curl -L -O https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/coco-resnet-101.index
+if command -v curl 1>/dev/null; then
+    DOWNLOADER="curl -L -O"
+else
+    DOWNLOADER="wget"
+fi
 
-curl -L -O https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/pairwise_coco.tar.gz
+$DOWNLOADER https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/coco-resnet-101.data-00000-of-00001
+$DOWNLOADER https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/coco-resnet-101.meta
+$DOWNLOADER https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/coco-resnet-101.index
+
+$DOWNLOADER https://datasets.d2.mpi-inf.mpg.de/deepercut-models-tensorflow/pairwise_coco.tar.gz
 tar xvzf pairwise_coco.tar.gz
--- models/pretrained/download.sh
+++ models/pretrained/download.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# Listed here https://github.com/tensorflow/models/tree/master/slim#pre-trained-models
+if command -v curl 1>/dev/null; then
+    DOWNLOADER="curl"
+else
+    DOWNLOADER="wget -qO-"
+fi
 
-curl http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz | tar xvz
-curl http://download.tensorflow.org/models/resnet_v1_101_2016_08_28.tar.gz | tar xvz
+$DOWNLOADER http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz | tar xvz
+$DOWNLOADER http://download.tensorflow.org/models/resnet_v1_101_2016_08_28.tar.gz | tar xvz
